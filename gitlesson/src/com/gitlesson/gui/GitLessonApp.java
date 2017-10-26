558da3e86240d7899039dcd9e29c870117097cf7
최경민 <artistckm86@gmail.com>
2017-10-26 15:57:26 +0900

    kbj 병합

--- gitlesson/src/com/gitlesson/gui/GitLessonApp.java
+++ gitlesson/src/com/gitlesson/gui/GitLessonApp.java
@@ -2,11 +2,8 @@ package com.gitlesson.gui;
 
 public class GitLessonApp {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		new CKMFrame();//CKM FRAME 호출
 		new HHJFrame();
-=======
 		new KBJFrame();
->>>>>>> origin/kbj
 	}
 }
