52f5fe15cd11a4a190e7c235f6f8c21c6785662c
김봉준 <mageclef@naver.com>
2017-10-26 16:27:31 +0900

    수정

--- gitlesson/src/com/gitlesson/gui/KBJFrame.java
+++ gitlesson/src/com/gitlesson/gui/KBJFrame.java
@@ -2,12 +2,17 @@ package com.gitlesson.gui;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class KBJFrame extends JFrame{
 		public KBJFrame() {
 			//this(); //Àç±Í
 			super("kbj frame");
+
+			JButton b1 = new JButton("Hit");
+			JButton b2 = new JButton("Stay");
+			
 			this.getContentPane().setBackground(new Color(0,255,255));
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setBounds(300, 300, 300, 300);
