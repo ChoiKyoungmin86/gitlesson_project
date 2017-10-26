c9d753988d71b9177ae2b14be5baf00d25278c91
유정 <kkokrkeka@gmail.com>
2017-10-26 15:47:55 +0900

    syj SYJFrame 생성

--- gitlesson/src/com/gitlesson/gui/SYJFrame.java
+++ gitlesson/src/com/gitlesson/gui/SYJFrame.java
@@ -1,10 +1,16 @@
 package com.gitlesson.gui;
 
-public class SYJFrame {
+import java.awt.Color;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
 
+public class SYJFrame extends JFrame {
+		public SYJFrame() {
+		//this();//재귀
+		super("syj frame");
+		this.getContentPane().setBackground(new Color(0,255,255));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(1000,600,300,200);
+		this.setVisible(true);
 	}
-
 }
