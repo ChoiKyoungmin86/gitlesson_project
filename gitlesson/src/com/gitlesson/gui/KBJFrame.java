3125389acde711b0db6834e66b6159d4e6d25834
김봉준 <mageclef@naver.com>
2017-10-26 16:58:58 +0900

    연습

--- gitlesson/src/com/gitlesson/gui/KBJFrame.java
+++ gitlesson/src/com/gitlesson/gui/KBJFrame.java
@@ -1,22 +1,40 @@
 package com.gitlesson.gui;
 
 import java.awt.Color;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class KBJFrame extends JFrame{
-		public KBJFrame() {
-			//this(); //Àç±Í
-			super("kbj frame");
+class KBJFrame extends JFrame {
+	JButton b1 = new JButton("Hit");
+	JButton b2 = new JButton("Stay");
+	
 
-			JButton b1 = new JButton("Hit");
-			JButton b2 = new JButton("Stay");
+		//this(); //ìž¬ê·€í•¨ìˆ˜
+		public KBJFrame() {
+			this.setLayout(new GridBagLayout());
+			GridBagConstraints gbc = new GridBagConstraints();
 			
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(5, 5, 5, 5);
+			
+			gbc.gridx = 0;
+			gbc.gridy = 0;
+			this.add(b1,gbc);
+			
+			gbc.gridx = 0;
+			gbc.gridy = 1;
+			this.add(b2,gbc);
+
 			this.getContentPane().setBackground(new Color(0,255,255));
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setBounds(300, 300, 300, 300);
 			this.setVisible(true);
 		}
-	}
+	
+}
+	
+
 
